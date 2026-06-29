1fb0d08921b3bcef55ab4cbacae08dec7240153f
Maria Estudiante <mariaestudiante53@gmail.com>
2026-06-29 02:16:42 -0600

    Eliminar importacion Navigate sin usar

--- frontend/src/paginas/Usuarios.js
+++ frontend/src/paginas/Usuarios.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Navigate } from "react-router-dom";
+import { useNavigate} from "react-router-dom";
 import API from "../api/api";
 import NavbarAdmin from "../componentes/NavbarAdmin";
 
